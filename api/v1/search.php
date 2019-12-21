de58f756f9e04460f552eabaeb176066a6135e11
Gabriel28 <nasc.gabriel.1997@gmail.com>
2019-12-21 19:58:01 -0300

    Nova Versão

---
+++ api/v1/search.php
@@ -0,0 +1,35 @@
+<?php
+
+define("MAX_RESULTS", 10);
+
+    if (isset($_GET['keyword']) )
+    {
+        $keyword = $_GET['keyword'];
+        $apikey = 'AIzaSyDR5jWbNxESBZNHmvJfrHl5WUN0ympNh0g'; 
+        $googleApiUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' . $keyword . '&maxResults=' . MAX_RESULTS . '&key=' . $apikey;
+        $videos = json_decode(file_get_contents($googleApiUrl));
+        $result = Array();
+        
+        foreach ($videos->items as $video) {
+            array_push($result, $video->snippet);
+        }
+        http_response_code(200);
+        header("Content-type:application-json");
+        echo json_encode($result);
+}
+    else if(isset($_GET['keyword'])==NULL)
+    {   
+        header("Content-type:application-json");
+        http_response_code(404);
+        // tell the user no products found
+        echo json_encode(array("type" => "error","message" => "Please enter the keyword."));
+    }
+    else
+    {
+        header("Content-type:application-json");
+        http_response_code(502);
+        // tell the user no products found
+        echo json_encode(array("type" => "error","message" => "Bad."));
+    }
+
+?>
